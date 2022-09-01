47917f4ffe80462c7ce6454714d7b2dd6cdd10de
nhab2 <n.habibollahi@yahoo.com>
2022-09-01 11:39:32 +0430

    FlunentAPI Configuration: Required fields and max length

--- Infrastructure/Data/Migrations/20220901060908_ProductTypeAndBrand.Designer.cs
+++ Infrastructure/Data/Migrations/20220901060908_ProductTypeAndBrand.Designer.cs
@@ -3,14 +3,16 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Api.Migrations
 {
     [DbContext(typeof(StoreDbContext))]
-    partial class StoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220901060908_ProductTypeAndBrand")]
+    partial class ProductTypeAndBrand
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Infrastructure/Data/StoreDbContext.cs
+++ Infrastructure/Data/StoreDbContext.cs
@@ -1,13 +1,33 @@
 using Core.Entities;
-using Microsoft.EntityFrameworkCore; 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection;
+
 namespace Infrastructure.Data
 {
-	public class StoreDbContext:DbContext
-	{
+    public class StoreDbContext:DbContext
+    {
 		public StoreDbContext(DbContextOptions<StoreDbContext> options):base(options)
 		{
 
 		}
 		public DbSet<Product> Products{get;set;}
-	}
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Id).IsRequired();
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Description).IsRequired().HasMaxLength(180);
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
+            builder.Property(p => p.PictureUrl).IsRequired();
+            //builder.Property(p => p.ProductBrand).WithMany().
+            //  HasForeignKey(p => p.ProductBrandId);
+            //builder.Property(p => p.ProductType).WithMany().
+            //  HasForeignKey(p => p.ProductBrandId);
+        }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
 }
