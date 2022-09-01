fab69f94d7205a650642f144d46bfb02180ea8bf
nhab2 <n.habibollahi@yahoo.com>
2022-09-01 09:41:01 +0430

    Initial Commit

---
+++ Api/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController:ControllerBase
+    {
+        private readonly StoreDbContext _context;
+        public ProductController(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            try
+            {
+                var products = await _context.Products.ToListAsync();
+                return Ok(products);
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message as string);
+            }
+        }
+
+        //https://localhost:5001/api/Product/2
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<Product>> GetProduct(int Id)
+        {
+            return await _context.Products.FindAsync(Id);
+        }
+    }
+
+}
---
+++ Infrastructure/Data/StoreDbContext.cs
@@ -0,0 +1,13 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore; 
+namespace Infrastructure.Data
+{
+	public class StoreDbContext:DbContext
+	{
+		public StoreDbContext(DbContextOptions<StoreDbContext> options):base(options)
+		{
+
+		}
+		public DbSet<Product> Products{get;set;}
+	}
+}
